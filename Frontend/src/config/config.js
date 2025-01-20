bd3405c919fc1fcf3c967366fb8a6ae476d65e38
Hitesh Bhoot <hiteshbhoot938@gmail.com>
2025-01-20 21:27:20 +0530

    `Update config.js to use new base_url`

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   // base_url: "http://localhost:8000",
-  base_url: "http://192.168.1.35:8000",
+  // base_url: "http://192.168.1.35:8000",
+  base_url: "https://uber-clone-backend-bnxw.onrender.com",
 };
 
 export default config;
